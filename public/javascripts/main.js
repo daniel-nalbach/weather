fb9e3c6fc41f1333148525445c7a8078e62ad0f1
Daniel Nalbach <thenalbachs@gmail.com>
2015-05-14 17:10:33 -0600

    first commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,15 @@
+$('document').ready(function() {
+  var tempC = Weather.main.temp - 273.15;
+  var tempF = Math.floor((tempC * (9/5)) + 32);
+  $('header h1').text(Weather.name);
+  $('header h3').text(Weather.sys.country);
+  $('main .current .status').text(Weather.weather[0].main);
+  $('main .current .temp').text(tempF + "°");
+  $('main .current .humidity').text(Weather.main.humidity + "%");
+  $('main .current .windSpeed').text(Weather.wind.speed);
+  $('main .current .windDirection').text(Weather.wind.deg);
+
+  if (Weather.weather[0].main === "Clear") {
+    $('main .current .card').addClass('clear');
+  }
+});
